fa68f8da68cb668e21cd0ad93ab55338ed695348
Peng <oncwnuFTUQjdybdxFBXBI5KsX74E@git.weixin.qq.com>
2022-04-07 13:06:11 +0800

    update

--- module/src/main/cpp/example.cpp
+++ module/src/main/cpp/example.cpp
@@ -51,6 +51,8 @@ private:
 static int urandom = -1;
 
 static void companion_handler(int i) {
+    LOGD("shockerhhh\n");
+
     if (urandom < 0) {
         urandom = open("/dev/urandom", O_RDONLY);
     }
